9c5d231e427840315c566f35baec175df44675fc
arrowalker3 <arrowalker3@gmail.com>
2020-06-19 09:31:27 -0600

    Added button to Create screens, and changed some spinners to EditText in CreateEmployee

--- app/src/main/java/com/example/equipme/MainActivity.java
+++ app/src/main/java/com/example/equipme/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.equipme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    public static final String throwawayKey = "THROW_KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String whatever;
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        whatever = sharedPreferences.getString(throwawayKey, null);
+    }
+
+    public void testPreferences(View view) {
+        String throwaway = "da bomb diggity";
+
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(throwawayKey, throwaway);
+        editor.commit();
+        Toast.makeText(MainActivity.this, "It's saved", Toast.LENGTH_SHORT).show();
     }
 }
