f4dadac6e1de1fa21377d050c3539b33eae05a31
arrowalker3 <arrowalker3@gmail.com>
2020-06-26 09:02:19 -0600

    Added employee and equipment lists to MainActivity

--- app/src/main/java/com/example/equipme/MainActivity.java
+++ app/src/main/java/com/example/equipme/MainActivity.java
@@ -8,7 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Employee> employeeList;
+    ArrayList<Equipment> equipmentList;
+
     SharedPreferences sharedPreferences;
     public static final String throwawayKey = "THROW_KEY";
 
