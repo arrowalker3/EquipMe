35e0e9cbdb43216e28db771199a99a6d2a1dd52e
KyanosTower <hdmmjohnson@gmail.com>
2020-06-29 11:31:54 -0500

    Added code for save & load, load still needs work.

--- app/src/main/java/com/example/equipme/MainActivity.java
+++ app/src/main/java/com/example/equipme/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.equipme;
 
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -42,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
         String whatever;
         sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         whatever = sharedPreferences.getString(throwawayKey, null);
+        //save(employeeList, equipmentList);
     }
 
     public void testPreferences(View view) {
@@ -55,12 +70,32 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this, "It's saved", Toast.LENGTH_SHORT).show();
     }
 
-    public void save(){
-        //save to an sd card? maybe?
-        //use it like an api?
+    public void save(ArrayList employeeList, ArrayList equipmentList){
+        Gson employee = new Gson();
+        Gson equipment = new Gson();
+        employee.toJson(employeeList);
+        equipment.toJson(equipmentList);
+        JSONArray employeeJSON = new JSONArray(employeeList);
+        JSONArray equipmentJSON = new JSONArray(equipmentList);
+        //This toast goes so it is getting made into Json
+        //Toast.makeText(MainActivity.this, "Wow its Json now.", Toast.LENGTH_SHORT).show()
+        //connect to database and save to it
+        //Load into a file on the database
+
     }
 
-    public void load(){
-
+    public void load() throws IOException {
+        //access database
+        //use mocky.io for tests
+        //load it in.
+        //link: https://run.mocky.io/v3/fc3ebf63-a032-4f1b-acf3-e65a23770054
+        URLConnection connection = new URL("https://run.mocky.io/v3/fc3ebf63-a032-4f1b-acf3-e65a23770054").openConnection();
+        InputStream responseStream = connection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(responseStream));
+        StringBuilder stringBuilder = new StringBuilder();
+        String hold;
+        while((hold = reader.readLine()) != null)
+            stringBuilder.append(hold);
+        stringBuilder.toString();
     }
 }
