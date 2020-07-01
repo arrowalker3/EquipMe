29a566efbc69aa4121ffe6cc5f7647caaeefe9df
arrowalker3 <arrowalker3@gmail.com>
2020-07-01 00:40:00 -0600

    Added styles to 'CreateEmployeeActivity.xml'. Changed layout to utilize a gridlayout. CreateEmployeeActivity finishes if the needed fields are all filled.

--- app/src/main/java/com/example/equipme/CreateEmployeeActivity.java
+++ app/src/main/java/com/example/equipme/CreateEmployeeActivity.java
@@ -2,17 +2,23 @@ package com.example.equipme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class CreateEmployeeActivity extends AppCompatActivity {
+    private Spinner jobTitleSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_employee);
+
+        // Fill Spinner with Job Title Options
     }
 
     /**************************************************************************
@@ -26,9 +32,12 @@ public class CreateEmployeeActivity extends AppCompatActivity {
      **************************************************************************/
     public void addEmployeeToList(View view) {
         if (allInformationFilled()) {
-            // Create Employee
-            // Send to MainActivity
-            // Go to MainActivity
+            // Check for unique ID
+
+                // Create Employee
+                // Send to MainActivity
+                // Go to MainActivity
+            finish();
         } else {
             String errorMessage = "Some information is still empty";
             Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
@@ -39,13 +48,34 @@ public class CreateEmployeeActivity extends AppCompatActivity {
     /**************************************************************************
      * ALL INFORMATION FILLED
      *
-     * Called when "Create Employe" button is pressed. Checks that all needed
-     * information has been filled, then adds a newly created Employee to the
-     * main list.
-     *
-     * @param view - Current view
+     * Checks each field in the current view except notes. If any of them are
+     * at their default value, returns false. Else returns true.
      **************************************************************************/
     private boolean allInformationFilled() {
-        return false;
+        // Check Name EditText
+        EditText name = (EditText) findViewById(R.id.employeeNameEditText);
+        if (name.getText().toString().equals("")) {
+            return false;
+        }
+
+        // Check Email EditText
+        EditText email = (EditText) findViewById(R.id.employeeEmailEditText);
+        if (email.getText().toString().equals("")) {
+            return false;
+        }
+
+        // Check ID EditText
+        EditText id = (EditText) findViewById(R.id.employeeIdEditText);
+        if (id.getText().toString().equals("")) {
+            return false;
+        }
+
+        // Check Job Title Spinner
+        Spinner jobTitle = findViewById(R.id.employeeJobTitleSpinner);
+        if (jobTitle.getSelectedItem().toString().equals("Choose one:")) {
+            return false;
+        }
+
+        return true;
     }
 }
