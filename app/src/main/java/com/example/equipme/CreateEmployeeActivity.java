a512348f47ad10bb36229bd7751882493a2dc7e4
arrowalker3 <arrowalker3@gmail.com>
2020-06-30 21:26:00 -0600

    Error message appears as toast if 'Create Employee' Button is pressed in 'CreateEmployeeActivity'

--- app/src/main/java/com/example/equipme/CreateEmployeeActivity.java
+++ app/src/main/java/com/example/equipme/CreateEmployeeActivity.java
@@ -2,8 +2,10 @@ package com.example.equipme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class CreateEmployeeActivity extends AppCompatActivity {
 
@@ -13,7 +15,37 @@ public class CreateEmployeeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_employee);
     }
 
+    /**************************************************************************
+     * ADD EMPLOYEE TO LIST
+     *
+     * Called when "Create Employe" button is pressed. Checks that all needed
+     * information has been filled, then adds a newly created Employee to the
+     * main list.
+     *
+     * @param view - Current view
+     **************************************************************************/
     public void addEmployeeToList(View view) {
+        if (allInformationFilled()) {
+            // Create Employee
+            // Send to MainActivity
+            // Go to MainActivity
+        } else {
+            String errorMessage = "Some information is still empty";
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
 
+    /**************************************************************************
+     * ALL INFORMATION FILLED
+     *
+     * Called when "Create Employe" button is pressed. Checks that all needed
+     * information has been filled, then adds a newly created Employee to the
+     * main list.
+     *
+     * @param view - Current view
+     **************************************************************************/
+    private boolean allInformationFilled() {
+        return false;
     }
 }
